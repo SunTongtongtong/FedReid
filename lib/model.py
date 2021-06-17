8e208c8026d1153542388a96e37c437e4be51add
Shitong <shitong.sun@qmul.ac.uk>
2021-06-17 10:17:56 +0100

    add senet backbone

--- lib/model.py
+++ lib/model.py
@@ -11,7 +11,7 @@ import torch
 
 # from .resnet50_AN import resnet50
 from .model_Gavin import resnet50
-
+from .se_resnet import se_resnet50
 # Weight initialisation
 def weights_init_kaiming(m):
     classname = m.__class__.__name__
@@ -72,6 +72,7 @@ class embedding_net(nn.Module):
         super(embedding_net, self).__init__()
         # model_backbone = models.resnet50(pretrained=True)
         model_backbone = resnet50(pretrained=True)
+        # model_backbone = se_resnet50(pretrained=True)
 
         model_backbone.avgpool = nn.AdaptiveAvgPool2d((1,1))
         self.model = model_backbone
